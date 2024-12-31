167d98dd1039db30643d757c8ba9a9a43bececa5
Vihananand <vihananand2018@gmail.com>
2024-12-31 22:16:10 +0530

    added abstraction.java

--- OOPS/abstraction.java
+++ OOPS/abstraction.java
@@ -1,3 +1,24 @@
+abstract class Animal{
+    abstract void walk();
+}
+
+class Horse extends Animal{
+    public void walk(){
+        System.out.println("Walks on 4 legs.");
+    }
+}
+
+class Gorilla extends Animal{
+    public void walk(){
+        System.out.println("Can walk on 2 legs.");
+    }
+}
+
 public class abstraction {
-    
+    public static void main(String[] args) {
+        Gorilla g1 = new Gorilla();
+        g1.walk();
+        Horse h1 = new Horse();
+        h1.walk();
+    }
 }
